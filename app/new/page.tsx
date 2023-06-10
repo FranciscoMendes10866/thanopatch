e41cdda194e8ec129d467e5f257abe4ec96ad000
FranciscoMendes10866 <fmendes_dev@protonmail.ch>
2023-06-11 00:53:41 +0100

    wip: data submission route became ssr

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,71 +1,61 @@
-"use client";
-
-import { useCallback } from "react";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { Form, Field } from "houseform";
+import { redirect } from "next/navigation";
+import { GraphQLClient, gql } from "graphql-request";
 import { z } from "zod";
 
-import { addTodo } from "./actions";
+const mutation = gql`
+  mutation addTodo($title: String!) {
+    addTodo(title: $title) {
+      id
+    }
+  }
+`;
 
-interface FormValues {
-  title: string;
+const formValuesSchema = z.object({
+  title: z.string().min(3),
+});
+
+export async function addTodo(formData: FormData) {
+  "use server";
+
+  const formValues = {} as any;
+  for (const [key, value] of [...formData.entries()]) {
+    if (key.includes("ACTION_ID")) continue;
+    formValues[key] = value.valueOf();
+  }
+
+  const parsed = await formValuesSchema.parseAsync(formValues);
+  const graphQLClient = new GraphQLClient("http://localhost:3000/api/graphql");
+  await graphQLClient.request(mutation, parsed);
+  redirect("/");
 }
 
 export default function Page() {
-  const router = useRouter();
-
-  const onSubmitHandler = useCallback(
-    async (values: FormValues) => {
-      await addTodo(values.title);
-      router.push("/");
-    },
-    [router]
-  );
-
   return (
     <div className="max-w-xs space-y-6">
       <Link href=".." className="btn btn-ghost">
         Go back
       </Link>
 
-      <Form<FormValues> onSubmit={onSubmitHandler}>
-        {({ submit }) => (
-          <div className="space-y-4">
-            <Field<FormValues["title"]>
-              name="title"
-              onBlurValidate={z.string().min(3)}
-            >
-              {({ value, setValue, onBlur, errors }) => (
-                <div>
-                  <label className="label">
-                    <span className="label-text">Task title</span>
-                  </label>
-                  <input
-                    type="text"
-                    value={value}
-                    onChange={(e) => setValue(e.target.value)}
-                    onBlur={onBlur}
-                    placeholder="Type here..."
-                    className="input input-bordered w-full max-w-xs"
-                  />
-                  <label className="label">
-                    {errors.map((error, index) => (
-                      <span key={index} className="label-text-alt">
-                        {error}
-                      </span>
-                    ))}
-                  </label>
-                </div>
-              )}
-            </Field>
+      <form action={addTodo} className="space-y-4">
+        <div>
+          <label className="label">
+            <span className="label-text">Task title</span>
+          </label>
+          <input
+            type="text"
+            name="title"
+            placeholder="Type here..."
+            className="input input-bordered w-full max-w-xs"
+            required
+            minLength={3}
+          />
+        </div>
 
-            <button className="btn btn-block" onClick={submit}>
-              Submit
-            </button>
-          </div>
-        )}
-      </Form>
+        <button className="btn btn-block" type="submit">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
